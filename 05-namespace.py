e783c585e9c9394a06f954d1909d2a5b2b40c7ee
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-09-04 16:45:05 +0900

    ch5. 네임스페이스

--- 05-namespace.py
+++ 05-namespace.py
@@ -107,20 +107,22 @@ siri.say_hi()  # 서로 동일
 
 print(dir(siri))
 """
-['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', 
-'__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__',
-'__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__',
-'__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 
-'cal_add', 'code', 'die', 'how_many', 'name', 'population', 'say_hi']
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', 
+'__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
+ '__init_subclass__','__le__', '__lt__', '__module__', '__ne__', '__new__', 
+ '__reduce__', '__reduce_ex__','__repr__', '__setattr__', '__sizeof__', 
+ '__str__', '__subclasshook__', '__weakref__', 'cal_add', 'code', 'die', 
+ 'how_many', 'name', 'population', 'say_hi']
 """
 
 print(dir(Robot))
 """
-['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', '__format__', 
-'__ge__', '__getattribute__', '__gt__', '__hash__', '__init__', '__init_subclass__', 
-'__le__', '__lt__', '__module__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', 
-'__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 
-'cal_add', 'die', 'how_many', 'population', 'say_hi']
+['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__',
+ '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', 
+ '__init__', '__init_subclass__', '__le__', '__lt__', '__module__', 
+ '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', 
+ '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 
+ 'cal_add', 'die', 'how_many', 'population', 'say_hi']
 """
 
 # ======================== __doc__ ======================================
